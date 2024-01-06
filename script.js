1018312553a40775d40fa0e8582b3cde8e895307
Catalin Gavrila <cata_bb92@yahoo.com>
2024-01-06 15:27:46 +0200

    huge improvements

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+// Debounce function to limit the rate at which a function is called
+    function debounce(func, delay) {
+      let timeoutId;
+      return function() {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(func, delay);
+      };
+    }
+    
+// JavaScript to change the class based on content height and window height
+    function adjustClass() {
+      var footer = document.getElementById("footer");
+      var contentHeight = document.body.clientHeight; // Height of the content
+      var windowHeight = window.innerHeight; // Height of the window
+
+      if (contentHeight < windowHeight) {
+        footer.className = "footer-fixed";
+      } else {
+        footer.className = "footer-relative";
+      }
+    }
+
+    // Initial adjustment on page load
+    adjustClass();
+
+    // Event listener for window resize with debounce
+    window.addEventListener("resize", debounce(adjustClass, 250)); // Adjust the delay as needed
